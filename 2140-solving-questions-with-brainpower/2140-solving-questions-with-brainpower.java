111d040061697fe9adf2f05b015826df7ac6fca9
Akarsh Anand <akarshanand2810@gmail.com>
2023-05-13 00:45:57 +0530

    Time: 6 ms (88.36%), Space: 95.6 MB (68.78%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,18 +1,33 @@
+// class Solution {
+//     long dp[];
+//     public long mostPoints(int[][] questions) {
+//         int n = questions.length;
+//         dp = new long[n];
+//         return dfs(questions, 0);
+//     }
+//     long dfs(int[][] questions, int i) {
+//         if(i >= questions.length)
+//             return 0;
+//         if(dp[i] != 0)
+//             return dp[i];
+//         long points = questions[i][0];
+//         int skip = questions[i][1];
+//         dp[i] = Math.max(points + dfs(questions, i + skip + 1), dfs(questions, i + 1));
+//         return dp[i];
+//     }
+// }
 class Solution {
-    long dp[];
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        dp = new long[n];
-        return dfs(questions, 0);
-    }
-    long dfs(int[][] questions, int i) {
-        if(i >= questions.length)
-            return 0;
-        if(dp[i] != 0)
-            return dp[i];
-        long points = questions[i][0];
-        int skip = questions[i][1];
-        dp[i] = Math.max(points + dfs(questions, i + skip + 1), dfs(questions, i + 1));
-        return dp[i];
+        long[] dp = new long[n];
+        dp[n - 1] = questions[n - 1][0];
+        for (int i = n - 2; i >= 0; --i) {
+            dp[i] = questions[i][0];
+            int skip = questions[i][1];
+            if(i + skip + 1 < n)
+                dp[i] += dp[i + skip + 1];
+            dp[i] = Math.max(dp[i], dp[i + 1]);
+        }
+        return dp[0];
     }
 }
